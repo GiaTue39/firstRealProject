56cecce9b1d1f77fd1d0c82e238702af59d33577
GiaTue <giatue39@gmail.com>
2020-07-27 09:35:34 +0700

    routing for signup form such as localhost:4200/signup

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { SignUpPageComponent } from './sign-up-page/sign-up-page.component';
+
+const routes: Routes = [{ path: 'signup', component: SignUpPageComponent }];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
