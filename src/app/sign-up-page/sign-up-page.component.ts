7b4278c1c2e9038d045ccec917c03a0532ecb2cb
GiaTue <giatue39@gmail.com>
2020-07-27 12:39:19 +0700

    add logo for Sign up Form

--- src/app/sign-up-page/sign-up-page.component.ts
+++ src/app/sign-up-page/sign-up-page.component.ts
@@ -1,26 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-// import {
-//   FormControl,
-//   FormGroupDirective,
-//   NgForm,
-//   Validators,
-// } from '@angular/forms';
-// import { ErrorStateMatcher } from '@angular/material/core';
-
-// export class MyErrorStateMatcher implements ErrorStateMatcher {
-//   isErrorState(
-//     control: FormControl | null,
-//     form: FormGroupDirective | NgForm | null
-//   ): boolean {
-//     const isSubmitted = form && form.submitted;
-//     return !!(
-//       control &&
-//       control.invalid &&
-//       (control.dirty || control.touched || isSubmitted)
-//     );
-//   }
-// }
-
 @Component({
   selector: 'app-sign-up-page',
   templateUrl: './sign-up-page.component.html',
@@ -30,13 +8,6 @@ export class SignUpPageComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {}
-
-  // emailFormControl = new FormControl('', [
-  //   Validators.required,
-  //   Validators.email,
-  // ]);
-
-  // matcher = new MyErrorStateMatcher();
 
   onSubmit(form) {
     console.log(form);
