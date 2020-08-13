ab8d3eb5f91bcd4d4c63429a0415577d3d44be8b
Truc Le <lethikimtruc38@gmail.com>
2020-08-13 17:07:12 +0700

    edit menu

--- src/app/components/layout/layout.component.ts
+++ src/app/components/layout/layout.component.ts
@@ -7,7 +7,7 @@ import { Router } from '@angular/router';
   styleUrls: ['./layout.component.css']
 })
 export class LayoutComponent implements OnInit {
-  hide : boolean = false;
+  hide: boolean = false;
   constructor(
     private router: Router
   ) { }
@@ -28,7 +28,7 @@ export class LayoutComponent implements OnInit {
     }
   ]
   images = [
-    
+
     'img/2.png',
     'img/3.png',
     'img/4.png',
@@ -36,34 +36,46 @@ export class LayoutComponent implements OnInit {
     'img/6.png',
     'img/7.png',
     'img/8.png',
-  ]
+  ];
 
-  onLogoutBtnClicked():void{
+  isMenuOpen = true;
+  menuSize = 'lg';
+
+  onLogoutBtnClicked(): void {
     localStorage.removeItem('token');
     this.router.navigate(['/signin']);
   }
 
   openNav() {
-    this.hide=true;
-    
-    document.querySelector("mySidenav").classList.add('');
-    
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("hide").style.display = "block";
+    this.isMenuOpen = true;
+    this.menuSize = 'lg';
+
+
+    this.hide = true;
+    // document.getElementById("close-menu-left").style.width = "250px";
+    // document.getElementById("fit-menu").style.display = "block";
+    // document.getElementById("close-menu-left").style.display = "block";
   }
 
   closeNav() {
-    this.hide=false;
-    document.getElementById("mySidenav").style.width = "100px";
-    document.getElementById("hide").style.display = "none";
-    document.getElementById("btnclose").style.display = "none";
-  } 
+    this.hide = false;
+    // document.getElementById("close-menu-left").style.width = "100px";
+
+    this.isMenuOpen = true;
+    this.menuSize = 'sm';
+    // document.getElementById("fit-menu").style.width="100%";
+    // document.getElementById("fit-menu").style.display = "none";
+    // document.getElementById("btnclose").style.display = "none";
+  }
 
   closeNav2() {
-    this.hide=false;
-    document.getElementById("mySidenav").style.width = "0px";
-    document.getElementById("hide").style.display = "none";
-    document.getElementById("btnclose").style.display = "none";
-  } 
+    this.hide = false;
+    this.isMenuOpen = false;
+    // document.getElementById("close-menu-left").style.width = "0px"; còn xuất hiện icon
+    // document.getElementById("close-menu-left").style.backgroundColor="red";
+    // document.getElementById("fit-menu").style.width="100%";
+    // document.getElementById("close-menu-left").style.display = "none";
+    // document.getElementById("btnclose").style.display = "none";
+  }
 
 }
